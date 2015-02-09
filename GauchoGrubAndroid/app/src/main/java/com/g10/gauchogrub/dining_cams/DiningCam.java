5ac9daeee43cf8c8ea2df47f91cfcccafa72e949
zakary <94zakary@gmail.com>
2015-02-09 12:12:33 -0800

    This is the merged project, with splash/Dynamic Menus/Cams

--- GauchoGrubAndroid/app/src/main/java/com/g10/gauchogrub/dining_cams/DiningCam.java
+++ GauchoGrubAndroid/app/src/main/java/com/g10/gauchogrub/dining_cams/DiningCam.java
@@ -1,26 +1,46 @@
 package com.g10.gauchogrub.dining_cams;
 
+import android.graphics.Bitmap;
+import com.g10.gauchogrub.io.WebUtils;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * DiningCams driver class.
  */
 public class DiningCam {
+
     public final static String Carrillo = "http://diningcams.housing.ucsb.edu/Image/Carrillo?";
     public final static String DeLaGuerra = "http://diningcams.housing.ucsb.edu/Image/DeLaGuerra?";
     public final static String Ortega = "http://diningcams.housing.ucsb.edu/Image/Ortega?";
-    //public final static String Portolla = "http://diningcams.housing.ucsb.edu/Image/Portolla?";
-    // Note, DiningCam doesn't exist for Portolla
-    private final static int delaySeconds = 2;
+    // There is no DiningCam for Portolla
 
-    private String url;
+    public final static Logger logger = Logger.getLogger("DiningCam");
 
-    public DiningCam(String diningCommon){
-        this.url = diningCommon;
+    private String partialUrl;
+    private WebUtils web;
+
+    public DiningCam(String diningCommonUrl) {
+        this.partialUrl = diningCommonUrl;
+        this.web = new WebUtils();
     }
 
-    private String getCurrentImage(){
-        // Returns a base URL concatinated with current UNIX time
-        return this.url + new Date().getTime();
+    private URL getCurrentImageUrl(){
+        // Base URL concatenated with current UNIX time
+        try {
+            return new URL(this.partialUrl + new Date().getTime());
+        } catch(MalformedURLException e) {
+            // Will never happen, but just in case...
+            logger.log(Level.INFO, e.toString());
+            return null;
+        }
+    }
+
+    public Bitmap getCurrentImage(int delay) {
+        URL currentUrl = this.getCurrentImageUrl();
+        return web.getDrawable(currentUrl, delay);
     }
 }
--- GauchoGrubAndroid/app/src/main/java/com/g10/gauchogrub/dining_cams/DiningCamsFragment.java
+++ GauchoGrubAndroid/app/src/main/java/com/g10/gauchogrub/dining_cams/DiningCamsFragment.java
@@ -1,20 +1,28 @@
 package com.g10.gauchogrub.dining_cams;
 
 import android.app.Fragment;
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
-import android.widget.SpinnerAdapter;
 
 import com.g10.gauchogrub.R;
 
-public class DiningCamsFragment extends Fragment implements AdapterView.OnItemSelectedListener {
+
+public class DiningCamsFragment extends Fragment implements AdapterView.OnItemSelectedListener, Runnable {
+
+    private DiningCam currentCam;
+    private Handler handler;
+    private ImageView imageView;
+    private final int delay = 10 * 1000;  // Milliseconds (10 seconds)
+    private boolean isOn;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -22,21 +30,76 @@ public class DiningCamsFragment extends Fragment implements AdapterView.OnItemSe
 
         // Initialize Spinner
         Spinner spinner = (Spinner)rootView.findViewById(R.id.dining_cams_spinner);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(), R.array.dining_commons_array, R.layout.dining_cams_spinner_item);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(), R.array.dining_cams_array, R.layout.dining_cams_spinner_item);
         adapter.setDropDownViewResource(R.layout.dining_cams_spinner_item);
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
         spinner.setSelection(0);
 
+        this.imageView = (ImageView) rootView.findViewById(R.id.dining_cam_image_view);
+        this.handler = new Handler();
+
         return rootView;
     }
 
+    @Override
     public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
         // An item was selected. You can retrieve the selected item using
         // parent.getItemAtPosition(pos)
-        // TODO: change dining cam
+        String[] camUrls = new String[]{DiningCam.Carrillo, DiningCam.DeLaGuerra, DiningCam.Ortega};
+        String camUrl = camUrls[pos];
+        this.currentCam = new DiningCam(camUrl);
+        this.startCam();
     }
 
+    @Override
+    public void onPause() {
+        super.onPause();
+        if(this.currentCam != null) {
+            this.stopCam();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if(this.currentCam != null) {
+            this.startCam();
+        }
+    }
+
+    private void startCam() {
+        this.isOn = true;
+        this.handler.post(this); // Update image right now
+    }
+
+    private void stopCam() {
+        this.isOn = false;
+        this.handler.removeCallbacks(this);
+    }
+
+    @Override
+    public void run() {
+        new AsyncTask<Void, Void, Bitmap>() {
+            @Override
+            protected Bitmap doInBackground(Void... v) {
+                return currentCam.getCurrentImage(delay);
+            }
+            @Override
+            protected void onPostExecute(Bitmap result){
+                imageView.setImageBitmap(result);
+                scheduleCamUpdate();
+            }
+        }.execute();
+    }
+
+    private void scheduleCamUpdate() {
+        if(isOn) {
+            handler.postDelayed(this, delay);
+        }
+    }
+
+    @Override
     public void onNothingSelected(AdapterView<?> parent) {
         // Another interface callback
     }
