52cc37ade05fde150cfab069c70e2c5862aa39f0
Victor Porter <porter614@gmail.com>
2015-02-19 18:27:52 -0800

    Deleted Settings Bar

--- GauchoGrubAndroid/app/src/main/java/com/g10/gauchogrub/BaseActivity.java
+++ GauchoGrubAndroid/app/src/main/java/com/g10/gauchogrub/BaseActivity.java
@@ -81,7 +81,7 @@ public class BaseActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main_menu, menu);
+        //getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
     }
 
