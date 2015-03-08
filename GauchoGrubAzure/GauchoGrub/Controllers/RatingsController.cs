e6164349e1840fbdf47b00c7a20ba25a4e1118f5
Jack <z_chiz@live.com>
2015-03-08 16:42:50 -0700

    T-46: Ratings controller

---
+++ GauchoGrubAzure/GauchoGrub/Controllers/RatingsController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using GauchoGrub.Models;
+
+namespace GauchoGrub.Controllers
+{
+    public class RatingsController : ApiController
+    {
+        private GauchoGrubContext db = new GauchoGrubContext();
+
+        // GET: api/Ratings
+        public IQueryable<Rating> GetRatings()
+        {
+            return db.Ratings;
+        }
+
+        // GET: api/Ratings/5
+        [ResponseType(typeof(Rating))]
+        public async Task<IHttpActionResult> GetRating(int id)
+        {
+            Rating rating = await db.Ratings.FindAsync(id);
+            if (rating == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(rating);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool RatingExists(int id)
+        {
+            return db.Ratings.Count(e => e.Id == id) > 0;
+        }
+    }
+}
