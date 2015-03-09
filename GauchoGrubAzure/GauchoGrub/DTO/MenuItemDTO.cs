9389999526c9f3818b5b9ad05c6fb1edbb9ea14b
Jack <z_chiz@live.com>
2015-03-08 18:39:03 -0700

    T-46: seed data

---
+++ GauchoGrubAzure/GauchoGrub/DTO/MenuItemDTO.cs
@@ -0,0 +1,23 @@
+﻿using GauchoGrub.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GauchoGrub.DTO
+{
+    public class MenuItemDTO
+    {
+        public int Id { get; set; }
+
+        public MenuCategory MenuCategory { get; set; }  // Navigation property
+       
+        public MenuItemType MenuItemType { get; set; }  // Navigation property
+
+        public String Title { get; set; }
+
+        public int TotalRatings { get; set; }
+
+        public int PositiveRatings { get; set; }
+    }
+}
