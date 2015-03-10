dd4d29cc494c372fabb6d926e22ec008e991fe40
Jack <z_chiz@live.com>
2015-03-09 19:06:03 -0700

    T-46: rating digest job improved

--- GauchoGrubAzure/RatingDigest/RatingDigestJob.cs
+++ GauchoGrubAzure/RatingDigest/RatingDigestJob.cs
@@ -8,73 +8,106 @@ using System.Diagnostics;
 
 namespace RatingDigest
 {
+    /*
+     * RatingDigestJob - web job that gets executed on a daily basis.
+     */
     public class RatingDigestJob
     {
+        /*
+         * Web job entry point.
+         */
         public static void Main()
         {
             // Execute job
             new RatingDigestJob().Start();
         }
 
+        /*
+         * Loops through all available UserRatings incrementing corresponding Ratings
+         * (by incrementing specific Ratings within the main loop).
+         * Also increments overall rating of corresponding MenuItems.
+         * UserRatings table gets cleared in the end.
+         */
         public void Start()
         {
-            Debug.WriteLine("Performing RatingDigest Job");
+            Log("Performing RatingDigest Job...");
 
             GauchoGrubContext db = new GauchoGrubContext();
-            
-            foreach (UserRating ur in db.UserRatings.ToList())
+
+            Log("Entering initialization loop...");
+            List<UserRating> userRatings = db.UserRatings.ToList();
+            List<Rating> ratings = new List<Rating>();
+            foreach (UserRating ur in userRatings)
             {
-                Debug.WriteLine("Processing UserRating (Id): " + ur.Id);
+                if (!ratings.Exists(r => r.MenuId == ur.MenuId && r.MenuItemId == ur.MenuItemId))
+                {
+                    Log("Creating Rating: " + ur.ToString());
+                    ratings.Add(new Rating { MenuId = ur.MenuId, MenuItemId = ur.MenuItemId });
+                }
+            }
+            Log("Exiting initialization loop...");
+
+
+            Log("Entering main loop...");
+            foreach (UserRating ur in userRatings)
+            {
+                Log("Processing UserRating: " + ur.ToString());
 
                 // Update Menu-specific item rating
-                Rating rating = GetOrCreateRating(ur.MenuId, ur.MenuItemId, db);
+                Rating rating = ratings.Single(r => r.MenuId == ur.MenuId && r.MenuItemId == ur.MenuItemId);
                 rating.TotalRatings += 1;
                 rating.PositiveRatings += (ur.PositiveRating) ? 1 : 0;
-                TryAddRating(rating, db);
-                Debug.WriteLine("Rating added/updated: " + rating.MenuId + " - MenuId, " + rating.MenuItemId + " - MenuItemId");
-
-                // Update MenuItem total overall rating
-                MenuItem item = db.MenuItems.SingleOrDefault(m => m.Id == ur.MenuItemId);
-                if (item != null)
-                {
-                    item.TotalRatings += 1;
-                    item.TotalPositiveRatings += (ur.PositiveRating) ? 1 : 0;
-                }
-                Debug.WriteLine("Updated MenuItem (Id):" + rating.MenuItemId);
+                Log("Rating updated successfully...");
 
                 // Remove UserRating
                 db.UserRatings.Remove(ur);
-                Debug.WriteLine("UserRating deleted (Id): " + ur.Id);
+                Log("UserRating deleted...");
             }
+            Log("Exiting main loop...");
+
+            Log("Entering post-processing loop...");
+            foreach (Rating rating in ratings)
+            {
+                AddOrUpdateRating(rating, db);
+                Log("Inserted/updated Rating in DB...");
+
+                // Update MenuItem overall rating
+                MenuItem item = db.MenuItems.SingleOrDefault(m => m.Id == rating.MenuItemId);
+                if (item != null)
+                {
+                    item.TotalRatings += rating.TotalRatings;
+                    item.TotalPositiveRatings += rating.PositiveRatings;
+                }
+                Log("Updated MenuItem overall rating...");
+            }
+            Log("Exiting post-processing loop...");
+
+            Log("Saving changes to DB...");
             db.SaveChanges();
+            Log("Changes saved successfully...");
+            Log("Exiting...");
         }
 
-        // Either fetches the corresponding Rating from the Database or creates a new one
-        private Rating GetOrCreateRating(int menuId, int menuItemId, GauchoGrubContext db)
+        // Adds or Updates a Rating
+        private void AddOrUpdateRating(Rating rating, GauchoGrubContext db)
         {
-            Rating rating = null;
+            // Try to fetch rating with corresponding MenuId and MenuItemId from the DB
             try
             {
-                rating = db.Ratings.Single(r => r.MenuId == menuId && r.MenuItemId == menuItemId);
+                Rating dbRating = db.Ratings.Single(r => r.MenuId == rating.MenuId && r.MenuItemId == rating.MenuItemId);
+                dbRating.TotalRatings += rating.TotalRatings;
+                dbRating.PositiveRatings += rating.PositiveRatings;
             }
             catch (Exception)
             {
-                rating = new Rating { MenuId = menuId, MenuItemId = menuItemId, PositiveRatings = 0, TotalRatings = 0 };
+                db.Ratings.Add(rating);
             }
-            return rating;
         }
 
-        // Tries adding a rating to DB
-        private void TryAddRating(Rating r, GauchoGrubContext db)
+        // Logs job progress
+        private void Log(String message)
         {
-            try
-            {
-                db.Ratings.Add(r);
-            }
-            catch (Exception)
-            {
-                // Do nothing
-            }
+            Console.WriteLine(message);
         }
     }
 }
